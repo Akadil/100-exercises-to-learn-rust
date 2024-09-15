478b9616247bbecbe34d43dc53b6934b19dd1bed
Akadil <akadil.kalimoldayev@gmail.com>
2024-09-15 16:25:15 +0200

    55th exercise finished

--- exercises/05_ticket_v2/12_thiserror/src/lib.rs
+++ exercises/05_ticket_v2/12_thiserror/src/lib.rs
@@ -3,12 +3,31 @@
 //   a `String` field into each variant.
 //   You'll also have to add `thiserror` as a dependency in the `Cargo.toml` file.
 
+// use thiserror;
+use std::fmt;
+
+#[derive(Debug, thiserror::Error, PartialEq)]
 enum TicketNewError {
+    #[error("Title cannot be empty")]
     TitleCannotBeEmpty,
+    #[error("Title cannot be longer than 50 bytes")]
     TitleTooLong,
+    #[error("Description cannot be empty")]
     DescriptionCannotBeEmpty,
+    #[error("Description cannot be longer than 500 bytes")]
     DescriptionTooLong,
 }
+
+// impl fmt::Display for TicketNewError {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         match self {
+//             TicketNewError::TitleCannotBeEmpty => write!(f, "Title cannot be empty"),
+//             TicketNewError::TitleTooLong => write!(f, "Title cannot be longer than 50 bytes"),
+//             TicketNewError::DescriptionCannotBeEmpty => write!(f, "Description cannot be empty"),
+//             TicketNewError::DescriptionTooLong => write!(f, "Description cannot be longer than 500 bytes"),
+//         }
+//     }
+// }
 
 #[derive(Debug, PartialEq, Clone)]
 struct Ticket {
--- exercises/05_ticket_v2/13_try_from/src/lib.rs
+++ exercises/05_ticket_v2/13_try_from/src/lib.rs
@@ -1,11 +1,39 @@
 // TODO: Implement `TryFrom<String>` and `TryFrom<&str>` for `Status`.
 //  The parsing should be case-insensitive.
 
+use std::convert;
+
 #[derive(Debug, PartialEq, Clone)]
 enum Status {
     ToDo,
     InProgress,
     Done,
+}
+
+impl convert::TryFrom<String> for Status {
+    type Error = ();
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "todo" => Ok(Status::ToDo),
+            "inprogress" => Ok(Status::InProgress),
+            "done" => Ok(Status::Done),
+            _ => Err(()),
+        }
+    }
+}
+
+impl convert::TryFrom<&str> for Status {
+    type Error = ();
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "todo" => Ok(Status::ToDo),
+            "inprogress" => Ok(Status::InProgress),
+            "done" => Ok(Status::Done),
+            _ => Err(()),
+        }
+    }
 }
 
 #[cfg(test)]
